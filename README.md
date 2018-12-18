2ecfe9866cdf4e716fff2f3c3186f98b15fbc32c
Sandro Marques <sandro123iv@gmail.com>
2018-12-18 19:55:56 +0000

    Added some documentation.

--- README.md
+++ README.md
@@ -1,14 +1,4 @@
-# jikan
+# jikan-dart
 
-Dart wrapper for the Jikan API. — https://jikan.moe
-
-## Getting Started
-
-This project is a starting point for a Dart
-[package](https://flutter.io/developing-packages/),
-a library module containing code that can be shared easily across
-multiple Flutter or Dart projects.
-
-For help getting started with Flutter, view our 
-[online documentation](https://flutter.io/docs), which offers tutorials, 
-samples, guidance on mobile development, and a full API reference.
+Dart wrapper for the [Jikan](https://jikan.moe) RESTful API, the unofficial API for MyAnimeList's website.
+This wrapper allows asynchronous requests to the API and a strongly-typed model.
--- lib/src/model.dart
+++ lib/src/model.dart
@@ -4,22 +4,32 @@ import 'package:json_annotation/json_annotation.dart';
 /// Rebuild with command: flutter packages pub run build_runner build
 part 'package:jikan/src/model.g.dart';
 
-
+/// Base mode class.
 abstract class MalBase {
+  /// ID associated with MyAnimeList.
   int malId;
+  /// URL to MyAnimeList's page.
   Uri url;
 
   MalBase({this.malId, this.url});
 }
 
+/// Stats model class.
 @JsonSerializable(fieldRename: FieldRename.snake)
 class MalStats {
+  /// Number of users watching this.
   final int watching;
+  /// Number of users that completed this.
   final int completed;
+  /// Number of users that marked this as "on-hold".
   final int onHold;
+  /// Number of users that dropped this.
   final int dropped;
+  /// Number of users that plan to watch this.
   final int planToWatch;
+  /// Number of total users.1
   final int total;
+  /// Distribution of scores, from 1 to 10
   final Map<String, MalScore> scores;
 
   MalStats({this.watching, this.completed, this.onHold, this.dropped,
@@ -29,9 +39,12 @@ class MalStats {
   Map<String, dynamic> toJson() => _$MalStatsToJson(this);
 }
 
+/// Score model class.
 @JsonSerializable(fieldRename: FieldRename.snake)
 class MalScore {
+  /// Number of votes.
   final int votes;
+  /// Relative percentage of votes.
   final double percentage;
 
   MalScore({this.votes, this.percentage});
@@ -40,8 +53,10 @@ class MalScore {
   Map<String, dynamic> toJson() => _$MalScoreToJson(this);
 }
 
+/// Forum model class.
 @JsonSerializable(fieldRename: FieldRename.snake)
 class MalForum {
+  /// List of forum topics.
   final List<MalForumTopic> topics;
 
   MalForum({this.topics});
@@ -50,11 +65,18 @@ class MalForum {
   Map<String, dynamic> toJson() => _$MalForumToJson(this);
 }
 
+/// Topic Post model class.
 @JsonSerializable(fieldRename: FieldRename.snake)
 class MalForumTopicPost {
+  /// Post's URL;
   final Uri url;
+  /// Author's name
+  // TODO move this into it's subclass?
   final String authorName;
+  /// Author's URL
+  // TODO move this into it's subclass?
   final Uri authorUrl;
+  /// Date of posting.
   final DateTime datePosted;
 
   MalForumTopicPost({this.url, this.authorName, this.authorUrl, this.datePosted});
@@ -63,11 +85,16 @@ class MalForumTopicPost {
   Map<String, dynamic> toJson() => _$MalForumTopicPostToJson(this);
 }
 
+/// Topic model class.
 @JsonSerializable(fieldRename: FieldRename.snake)
 class MalForumTopic extends MalForumTopicPost {
+  /// Topic's ID on MyAnimeList.
   final int topicId;
+  /// Topic's title;
   final String title;
+  /// Topic's numbr of replies.
   final int replies;
+  /// Topic's last post.
   final MalForumTopicPost lastPost;
 
   MalForumTopic({this.topicId, this.title, this.replies, this.lastPost});
@@ -76,44 +103,88 @@ class MalForumTopic extends MalForumTopicPost {
   Map<String, dynamic> toJson() => _$MalForumTopicToJson(this);
 }
 
+/// Anime model class.
 /// Endpoint: /anime/<id>
 @JsonSerializable(fieldRename: FieldRename.snake)
 class Anime extends MalBase {
-  final Uri imageUrl;
-  final Uri trailerUrl;
+  /// Title of the anime.
   final String title;
+  /// Title of the anime in English.
   final String titleEnglish;
+  /// Title of the anime in Japanese.
   final String titleJapanese;
+  /// Anime's multiple titles (if any).
   final List<String> titleSynonyms;
+  /// Anime's image URL.
+  final Uri imageUrl;
+  /// Anime's trailer URL.
+  final Uri trailerUrl;
+  /// Anime type (e. g. "TV", "Movie").
+  // TODO: convert to enum?
   final String type;
+  /// Anime source (e .g. "Manga" or "Original").
+  // TODO: convert to enum?
   final String source;
+  /// Anime's episode count.
   final int episodes;
   final String status;
+  /// Is anime currently airing.
   final bool airing;
+  /// Date when anime started/will start airing.
+  final DateTime airingStart;
+  /// Time period of starting and ending anime dates.
   final AnimeAired aired;
+  /// Anime's duration per episode.
   @JsonKey(fromJson: _durationFrom, toJson: _durationTo)
   final Duration duration;
+  /// Anime's age rating.
   final String rating;
+  /// Anime's score on MyAnimeList, up to 2 decimal places.
   final double score;
+  /// Number of people the anime has been scored by.
   final int scoredBy;
+  /// Anime rank on MyAnimeList (score).
+  final int rank;
+  /// Anime popularity rank on MyAnimeList.
   final int popularity;
+  /// Anime members count on MyAnimeList.
   final int members;
+  /// Anime favourite count on MyAnimeList.
   final int favorites;
+  /// Anime's synopsis.
   final String synopsis;
+  /// Anime's background info. Empty if don't have any.
   final String background;
+  /// Season and year the anime premiered.
+  // TODO: break down into own subclass?
   final String premiered;
+  /// Anime broadcast day and timings (usually JST).
+  // TODO: attempt parsing into own subclass
   final String broadcast;
+  /// Anime's related items (anime, manga, spin offs, etc.)
   final Map<String, List<Anime>> related;
+  /// Anime's producers.
   final List<Company> producers;
+  /// Anime's licensors.
   @JsonKey(fromJson: _licensorsFrom)
   final List<Company> licensors;
+  /// Anime's studio(s) numerically indexed with array values.
   final List<Company> studios;
+  /// Anime's genres.
   final List<Genre> genres;
+  /// Anime's opening themes' names and author.
+  // TODO: attempt parsing into own subclass
   final List<String> openingThemes;
+  /// Anime's ending themes' names and author.
+  // TODO: attempt parsing into own subclass
   final List<String> endingThemes;
 
+  /// Is anime suitable for kids.
   final bool r18; // Seen in the schedule
+  /// Is anime marked for adult audiences only.
   final bool kids; // Seen in the schedule
+  /// Is anime continuing from last season.
+  final bool continued; // Seen in the schedule
 
   /// Endpoint: /anime/<id>/characters_staff
   final Characters characters;
@@ -140,13 +211,14 @@ class Anime extends MalBase {
   final MalForum moreInfo;
 
 
-  Anime({this.related, this.producers, this.licensors, this.studios, this.titleSynonyms, this.type,
-      this.source, this.status, this.airing, this.aired, this.duration, this.rating, this.score,
-      this.scoredBy, this.popularity, this.members, this.favorites, this.synopsis, this.background,
-      this.premiered, this.broadcast, this.genres, this.openingThemes, this.endingThemes, this.characters,
-      this.imageUrl, this.trailerUrl, this.title, this.titleEnglish, this.titleJapanese, this.articles,
-      this.episodes, this.pictures, this.videos, this.r18, this.kids, this.stats, this.forum, this.moreInfo,
-      this.episodeList});
+  Anime({this.title, this.titleEnglish, this.titleJapanese, this.titleSynonyms,
+      this.imageUrl, this.trailerUrl, this.type, this.source, this.episodes, this.status,
+      this.airing, this.airingStart, this.aired, this.duration, this.rating, this.score,
+      this.scoredBy, this.rank, this.popularity, this.members, this.favorites, this.synopsis,
+      this.background, this.premiered, this.broadcast, this.related, this.producers,
+      this.licensors, this.studios, this.genres, this.openingThemes, this.endingThemes,
+      this.r18, this.kids, this.continued, this.characters, this.articles, this.episodeList,
+      this.pictures, this.videos, this.stats, this.forum, this.moreInfo});
 
   factory Anime.fromJson(Map<String, dynamic> json) => _$AnimeFromJson(json);
   Map<String, dynamic> toJson() => _$AnimeToJson(this);
@@ -177,9 +249,12 @@ class Anime extends MalBase {
   }
 }
 
+/// Episodes model class.
 @JsonSerializable(fieldRename: FieldRename.snake)
 class AnimeEpisodes {
+  /// Number of the last page of episodes. Pagination starts to occur past the 100 episodes.
   final int episodesLastPage;
+  /// List of episodes.
   final List<AnimeEpisode> episodes;
 
   AnimeEpisodes({this.episodesLastPage, this.episodes});
@@ -188,16 +263,26 @@ class AnimeEpisodes {
   Map<String, dynamic> toJson() => _$AnimeEpisodesToJson(this);
 }
 
+/// Episode model class.
 @JsonSerializable(fieldRename: FieldRename.snake)
 class AnimeEpisode {
+  /// Episode's ID, on MyAnimeList.
   final int episodeId;
+  /// Episode's title.
   final String title;
+  /// Episode's title in japanese.
   final String titleJapanese;
+  /// Episode's title in romanji.
   final String titleRomanji;
+  /// Episode's airing date.
   final AnimeAired aired;
+  /// Is filler episode.
   final bool filler;
+  /// Is recap episode.
   final bool recap;
+  /// Episode's video URL.
   final Uri videoUrl;
+  /// Episode's forum URL.
   final Uri forumUrl;
 
   AnimeEpisode({this.episodeId, this.title, this.titleJapanese,
@@ -208,11 +293,13 @@ class AnimeEpisode {
   Map<String, dynamic> toJson() => _$AnimeEpisodeToJson(this);
 }
 
+/// Aired model class.
 @JsonSerializable(fieldRename: FieldRename.snake)
 class AnimeAired {
+  /// Airing start date.
   final DateTime from;
+  /// Airing end date.
   final DateTime to;
-
 
   AnimeAired({this.from, this.to});
 
@@ -220,11 +307,13 @@ class AnimeAired {
   Map<String, dynamic> toJson() => _$AnimeAiredToJson(this);
 }
 
+/// Genre model class.
 @JsonSerializable(fieldRename: FieldRename.snake)
 class Genre extends MalBase {
+  /// Genre's type
   final String type;
+  /// Genre's name
   final String name;
-
 
   Genre({this.type, this.name});
 
@@ -232,8 +321,10 @@ class Genre extends MalBase {
   Map<String, dynamic> toJson() => _$GenreToJson(this);
 }
 
+/// Anime's characters and staff model class.
 @JsonSerializable(fieldRename: FieldRename.snake)
 class Characters {
+  /// Anime's characters collection with basic information.
   final List<Character> characters;
 
   Characters({this.characters});
@@ -242,13 +333,16 @@ class Characters {
   Map<String, dynamic> toJson() => _$CharactersToJson(this);
 }
 
+/// Character model class.
 @JsonSerializable(fieldRename: FieldRename.snake)
 class Character extends MalBase {
+  /// Character's image URL.
   final Uri imageUrl;
+  /// Character's name.
   final String name;
   final String role;
+  /// Character's voice actors.
   final List<VoiceActor> voice_actors;
-
 
   Character({this.imageUrl, this.name, this.role, this.voice_actors});
 
@@ -256,11 +350,13 @@ class Character extends MalBase {
   Map<String, dynamic> toJson() => _$CharacterToJson(this);
 }
 
+/// Company model class.
 @JsonSerializable(fieldRename: FieldRename.snake)
 class Company extends MalBase {
+  /// Company's type.
   final String type;
+  /// Company's name.
   final String name;
-
 
   Company({this.type, this.name});
 
@@ -268,11 +364,13 @@ class Company extends MalBase {
   Map<String, dynamic> toJson() => _$CompanyToJson(this);
 }
 
+/// Person model class.
 @JsonSerializable(fieldRename: FieldRename.snake)
 class Person extends MalBase {
+  /// Person's name.
   final String name;
+  /// Person's image URL.
   final Uri imageUrl;
-
 
   Person({this.name, this.imageUrl});
 
@@ -280,10 +378,11 @@ class Person extends MalBase {
   Map<String, dynamic> toJson() => _$PersonToJson(this);
 }
 
+/// Voice Actor model class.
 @JsonSerializable(fieldRename: FieldRename.snake)
 class VoiceActor extends Person {
+  /// Voice actor's used language.
   final String language;
-
 
   VoiceActor({this.language});
 
@@ -291,10 +390,11 @@ class VoiceActor extends Person {
   Map<String, dynamic> toJson() => _$VoiceActorToJson(this);
 }
 
+/// Staff model class.
 @JsonSerializable(fieldRename: FieldRename.snake)
 class Staff extends Person {
+  /// Staff's positions (e.g.).
   final List<String> positions;
-
 
   Staff({this.positions});
 
@@ -302,8 +402,10 @@ class Staff extends Person {
   Map<String, dynamic> toJson() => _$StaffToJson(this);
 }
 
+/// Articles model class.
 @JsonSerializable(fieldRename: FieldRename.snake)
 class Articles {
+  /// List of articles.
   final List<Article> articles;
 
   Articles({this.articles});
@@ -312,18 +414,29 @@ class Articles {
   Map<String, dynamic> toJson() => _$ArticlesToJson(this);
 }
 
+/// Article model class.
 @JsonSerializable(fieldRename: FieldRename.snake)
 class Article {
+  /// Article's URL.
   final Uri url;
+  /// Article's title.
   final String title;
+  /// Article's posting date.
   final DateTime date;
+  /// Author's name
+  // TODO subclass
   final String authorName;
+  /// Author's URL.
+  // TODO subclass
   final Uri authorUrl;
+  /// Article's forum URL.
   final Uri forumUrl;
+  /// Article's image URL.
   final Uri imageUrl;
+  /// Article's number of comments.
   final int comments;
+  /// Article's summary.
   final String intro;
-
 
   Article({this.url, this.title, this.date, this.authorName, this.authorUrl,
       this.forumUrl, this.imageUrl, this.comments, this.intro});
@@ -332,8 +445,10 @@ class Article {
   Map<String, dynamic> toJson() => _$ArticleToJson(this);
 }
 
+/// Pictures model class.
 @JsonSerializable(fieldRename: FieldRename.snake)
 class Pictures {
+  /// List of pictures.
   final List<Picture> pictures;
 
   Pictures({this.pictures});
@@ -342,11 +457,13 @@ class Pictures {
   Map<String, dynamic> toJson() => _$PicturesToJson(this);
 }
 
+/// Picture model class.
 @JsonSerializable(fieldRename: FieldRename.snake)
 class Picture {
+  /// Picture's high-resultion variant URL.
   final Uri large;
+  /// Picture's low-resultion variant URL.
   final Uri small;
-
 
   Picture({this.large, this.small});
 
@@ -354,12 +471,15 @@ class Picture {
   Map<String, dynamic> toJson() => _$PictureToJson(this);
 }
 
+/// Video model class.
 @JsonSerializable(fieldRename: FieldRename.snake)
 class Video {
+  /// Video's title.
   final String title;
+  /// Video's thumbnail URL.
   final Uri imageUrl;
+  /// Video's URL.
   final Uri videoUrl;
-
 
   Video({this.title, this.imageUrl, this.videoUrl});
 
@@ -367,17 +487,27 @@ class Video {
   Map<String, dynamic> toJson() => _$VideoToJson(this);
 }
 
+/// Schedule model class.
 /// Endpoint: /schedule/<?day>
 @JsonSerializable(fieldRename: FieldRename.snake)
 class Schedule {
+  /// List of anime which episodes are airing on monday.
   final List<Anime> monday;
+  /// List of anime which episodes are airing on tuesday.
   final List<Anime> tuesday;
+  /// List of anime which episodes are airing on wednesday.
   final List<Anime> wednesday;
+  /// List of anime which episodes are airing on thursday.
   final List<Anime> thursday;
+  /// List of anime which episodes are airing on friday.
   final List<Anime> friday;
+  /// List of anime which episodes are airing on saturday.
   final List<Anime> saturday;
+  /// List of anime which episodes are airing on sunday.
   final List<Anime> sunday;
+  /// List of anime which episodes are airing sometime during this week.
   final List<Anime> other;
+  /// List of anime with unknown airing date for this week.
   final List<Anime> unknown;
 
   Schedule({this.monday, this.tuesday, this.wednesday, this.thursday, this.friday, this.saturday, this.sunday, this.other, this.unknown});
